daafbfc482966466ed378c9d2611183536d917ba
Marcel <mytelefono0001@gmail.com>
2021-07-09 14:52:53 -0500

    pruebbba

--- pag-react-vlstt/src/App.js
+++ pag-react-vlstt/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          HANS MONZON INDUSTRIES. international
+          HANS MONZON INDUSTRIES.
         </a>
       </header>
     </div>
